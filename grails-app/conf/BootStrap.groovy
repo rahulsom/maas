702c770adb326b467e4a61ea3de34312d8d52046
Rahul Somasunderam <rahul.som@gmail.com>
2014-09-07 15:03:41 -0700

    Cleaner bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,27 +1,22 @@
-import com.github.rahulsom.maas.Loinc
 import com.github.rahulsom.maas.auth.Role
 import com.github.rahulsom.maas.auth.User
 import com.github.rahulsom.maas.auth.UserRole
-import org.apache.commons.lang.StringUtils
-import org.h2.tools.Csv
-
-import java.text.SimpleDateFormat
 
 class BootStrap {
-
-  def elasticSearchService
 
   def init = { servletContext ->
 
     User.withNewTransaction {
-      def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-      def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+      if (!Role.count) {
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 
-      def admin = new User(username: 'admin', password: 'admin').save(flush: true)
-      def user = new User(username: 'user', password: 'user').save(flush: true)
+        def admin = new User(username: 'admin', password: 'admin').save(flush: true)
+        def user = new User(username: 'user', password: 'user').save(flush: true)
 
-      UserRole.create admin, adminRole, true
-      UserRole.create user, userRole, true
+        UserRole.create admin, adminRole, true
+        UserRole.create user, userRole, true
+      }
     }
 
   }
