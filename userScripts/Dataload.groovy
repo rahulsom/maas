d7d9f2be65d7f5d097052c809c28416b2585ded9
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-10-14 20:33:38 -0700

    Fixed dataDir

--- userScripts/Dataload.groovy
+++ userScripts/Dataload.groovy
@@ -1,6 +1,6 @@
 import com.github.rahulsom.maas.NdcProductController
 
 def service = ctx.getBean('dataService')
-def dataHome = "/Users/rahul/data"
+def dataHome = "/opt"
 service.storeLoinc("$dataHome/loinc/loinc.csv")
 service.storeNdc("$dataHome/ndc/product.txt", "$dataHome/ndc/package.txt", NdcProductController.errata, NdcProductController.badIds)
