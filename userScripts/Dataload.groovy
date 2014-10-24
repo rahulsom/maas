40e8a8d8739d68277fbe1726b19e2bb008a0bb68
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-10-24 11:30:45 -0700

    Improved Dataload

--- userScripts/Dataload.groovy
+++ userScripts/Dataload.groovy
@@ -2,14 +2,28 @@ import com.github.rahulsom.maas.NdcProductController
 
 def service = ctx.getBean('dataService')
 def dataHome = "/opt"
-if (new File("data/loinc.sha1").text != "sha1sum $dataHome/loinc/loinc.csv".execute().inputStream.text) {
-  service.storeLoinc("$dataHome/loinc/loinc.csv")
-  "sha1sum $dataHome/loinc/loinc.csv > data/loinc.sha1".execute()
+boolean shaMatches(String shaName, String fileName) {
+  def shaFile = new File("data/loinc.sha1")
+  if (!shaFile.exists()) {
+    return false
+  }
+  def fileSha = shaFile.text
+  def trueSha = "sha1sum $fileName | cut -d " " -f 1".execute.inputStream.text
+  return fileSha == trueSha
 }
-if (new File("data/product.sha1").text != "sha1sum $dataHome/ndc/product.txt".execute().inputStream.text ||
-    new File("data/package.sha1").text != "sha1sum $dataHome/ndc/package.txt".execute().inputStream.text
-) {
+
+def updateSha(String shaName, String fileName) {
+  def shaFile = new File("data/loinc.sha1")
+  def trueSha = "sha1sum $fileName | cut -d " " -f 1".execute.inputStream.text
+  shaFile.text = trueSha
+}
+if (!shaMatches("data/loinc.sha1", "$dataHome/loinc/loinc.csv")) {
+  service.storeLoinc("$dataHome/loinc/loinc.csv")
+  updateSha("data/loinc.sha1", "$dataHome/loinc/loinc.csv")
+}
+if (!shaMatches("data/product.sha1", "$dataHome/ndc/product.txt") ||
+    !shaMatches("data/package.sha1", "$dataHome/ndc/package.txt")) {
   service.storeNdc("$dataHome/ndc/product.txt", "$dataHome/ndc/package.txt", NdcProductController.errata, NdcProductController.badIds)
-  "sha1sum $dataHome/ndc/package.txt > data/package.sha1".execute()
-  "sha1sum $dataHome/ndc/product.txt > data/product.sha1".execute()
+  updateSha("data/product.sha1", "$dataHome/ndc/product.txt")
+  updateSha("data/package.sha1", "$dataHome/ndc/package.txt")
 }
